e348d0a5964ea8030fed49b91067aa7c1f575b11
oleebanerjee1 <obanerjee@potomacschool.org>
2022-11-04 08:50:55 -0400

    continuous movement works

--- pygameTestFile.py
+++ pygameTestFile.py
@@ -1,5 +1,38 @@
 import pygame
-
 pygame.init()
 
-# hi
+window = pygame.display.set_mode((500, 500))
+pygame.display.set_caption("Continuous Movement Test")
+
+x = 50
+y = 50
+width = 40
+height = 60
+vel = 5
+key = 0
+run = True
+
+while run:
+    pygame.time.delay(100)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+        if event.type == pygame.KEYDOWN:
+            key = event.key
+            #pygame.key.get_pressed()
+
+    if key == pygame.K_LEFT:
+        x -= vel
+    if key == pygame.K_RIGHT:
+        x += vel
+    if key == pygame.K_UP:
+        y -= vel
+    if key == pygame.K_DOWN:
+        y += vel
+
+    window.fill((0, 0, 0))  # Fills the screen with black
+    pygame.draw.rect(window, (255, 0, 0), (x, y, width, height))
+    pygame.display.update()
+
+pygame.quit()
