92edb5cdc338937f92c2f8d1c84eb4d0973b06b3
kyliemckinley01 <kmckinley@potomacschool.org>
2022-11-10 09:20:39 -0500

    there are now lives at the right hand corner that disspear as you die

--- pygameTestFile.py
+++ pygameTestFile.py
@@ -6,32 +6,37 @@ displayY = 500
 window = pygame.display.set_mode((displayX, displayY))
 pygame.display.set_caption("Continuous Movement Test")
 
-x = 230
-y = 230
-width = 20
-height = 20
+
+width = 40
+height = 40
+x = displayX/2 - width/2
+y = displayY/2 - height/2
 vel = 5
 key = 0
 pointArr = [(x + width/2, y + height/2), (x + width, y + height), (x + width, y)]
 run = True
-time = 0
-mazeLayout = """
-WWWWWWWWWW
-W   W    W
-W WWWWWW W
-W      W W
-WWW WWWWWW
-W W W  W W
-W   W    W
-W WWW W  W
-W     W  W
-WWWWWWWWEW
-""".splitlines()[1:]
+pastX = 0
+pastY = 0
+lifeColor = [(255, 0, 0), (255, 0, 0), (255, 0, 0)]
+numDeaths = 0
+died = False
+#mazeLayout = """
+#WWWWWWWWWW
+#W   W    W
+#W WWWWWW W
+#W      W W
+#WWW WWWWWW
+#W W W  W W
+#W   W    W
+#W WWW W  W
+#W     W  W
+#WWWWWWWWEW
+#""".splitlines()[1:]
 
-block = pygame.image.load("C:\\Users\\olee1\\Pictures\\block.png").convert()
+#block = pygame.image.load("C:\\Users\\olee1\\Pictures\\block.png").convert()
 
 # Using blit to copy content from one surface to other
-window.blit(block, (0, 0))
+#window.blit(block, (0, 0))
 
 # paint screen one time
 pygame.display.flip()
@@ -59,15 +64,13 @@ while run:
     if key == pygame.K_DOWN:
         y += vel
         pointArr = mouthDown()
-    window.fill((0, 0, 0))  # Fills the screen with black
+    pygame.draw.ellipse(window, (0, 0, 0), (pastX, pastY, width, height)) # past character turns black to user can't see
     pygame.draw.ellipse(window, (255, 234, 0), (x, y, width, height))
+    pastX = x
+    pastY = y
     pygame.draw.polygon(window, (0, 0, 0), (pointArr[0], pointArr[1], pointArr[2]))
-    # quick blurb of instructions for a short amount of time
-    time += 1
-    if time < 50:
-        font = pygame.font.Font('freesansbold.ttf', 19)
-        text = font.render('Use the arrow keys to move your avatar up and down.', True, (255, 0, 0))
-        window.blit(text, (0, 0))
+    lives = [pygame.draw.ellipse(window, lifeColor[0], (displayX-20, 10, 10, 10)), pygame.draw.ellipse(window, lifeColor[1], (displayX-35, 10, 10, 10)),pygame.draw.ellipse(window, lifeColor[2], (displayX-50, 10, 10, 10))]
+
     # Printing the mouth in different directions
     def mouthDown():
         point1 = (x + width/2, y + height/2)
@@ -93,8 +96,21 @@ while run:
         point3 = (x, y)
         arr = [point1, point2, point3]
         return arr
-    # If it reaches the boundaries of the screen stop and print game over
-    if x < 0 or y < 0 or x+width == displayX or y+height == displayY:
+    # If it reaches the boundaries of the screen set one circle to black
+    if x < 0 or y < 0 or x + width == displayX or y + height == displayY:
+        numDeaths += 1
+        died = True
+        if numDeaths <= 3:
+            print("reached")
+            print("NumDeaths:", str(numDeaths - 1))
+            lifeColor[numDeaths - 1] = (0, 0, 0)
+            print(str(lifeColor))
+
+    if died: #if it has died return to center of screen
+        x = displayX / 2 - width / 2
+        y = displayY / 2 - height / 2
+        died = False
+    def gameOver():
         vel = 0
         font = pygame.font.Font('freesansbold.ttf', 50)
         text = font.render('Game Over', True, (255, 0, 0))
