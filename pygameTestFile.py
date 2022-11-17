15dc291f3f9490277b027765dbf277a3f7ed9acc
oleebanerjee1 <obanerjee@potomacschool.org>
2022-11-16 20:33:01 -0500

    implemented maze into main fie, but pacman is currently eating the walls as he goes. not good.

--- pygameTestFile.py
+++ pygameTestFile.py
@@ -1,8 +1,8 @@
 import pygame
 pygame.init()
 
-displayX = 500
-displayY = 500
+displayX = 640
+displayY = 640
 window = pygame.display.set_mode((displayX, displayY))
 pygame.display.set_caption("Continuous Movement Test")
 
@@ -20,6 +20,42 @@ pastY = 0
 lifeColor = [(255, 0, 0), (255, 0, 0), (255, 0, 0)]
 numDeaths = 0
 died = False
+
+walls = []
+mazeLayout = """
+WWWWWWWWWW
+W   W    W
+W WWWWWW W
+W      W W
+WWW WWWWWW
+W W W  W W
+W   W    W
+W WWW W  W
+W     W  W
+WWWWWWWWEW
+""".splitlines()[1:]
+print(mazeLayout)
+
+block = pygame.image.load("C:\\Users\\olee1\\Pictures\\block.png").convert()
+
+# Using blit to copy content from one surface to other
+window.blit(block, (0, 0))
+
+for row in range(0, 10):
+    for column in range(0, 10):
+        if mazeLayout[row][column:column+1] == "W":
+            walls.append([column*64, row*64])
+            print(walls)
+        if mazeLayout[row][column:column+1] == "E":
+            exit = [column*64, row*64]
+            print(exit)
+
+for i in range(0, len(walls)):
+    window.blit(block, (walls[i][0], walls[i][1]))
+    print(walls[i][0], walls[i][1])
+
+# paint screen one time
+pygame.display.flip()
 
 while run:
     pygame.time.delay(100)
